0757630ad5390ae8b73c4e149f1cca7d109b2c46
Amelie-Ga <AmelieGa@outlook.com>
2021-09-14 10:56:51 +0200

    downloadbtn text B&W by contrast

--- components/BlockDownload.js
+++ components/BlockDownload.js
@@ -6,11 +6,12 @@ import Link from 'next/link';
 const BlockDownload = ({ blok }) => {
 
    return (
+     
       <SbEditable content={blok}>
          <span className="block mt-8 mb-16 border border-gray-200 rounded-md shadow-lg">
             <span className="flex flex-col p-8 md:block md:clear" >
                <Link href={blok.link}>
-                  <a target="_blank" className="order-2 w-full px-4 py-2 font-bold text-center text-white rounded-md md:float-right md:w-auto md:px-8" style={{ backgroundColor: blok.background_color.color}}>
+                  <a target="_blank" className="order-2 w-full px-4 py-2 font-bold text-center rounded-md md:float-right md:w-auto md:px-8" style={{ backgroundColor: blok.background_color.color, color: getContrast(blok.background_color.color)}}>
                      Afficher le dossier
                   </a>
                </Link>
@@ -19,8 +20,16 @@ const BlockDownload = ({ blok }) => {
             {blok.description && <span className="block px-8 py-4 text-sm text-gray-500 uppercase bg-gray-100 border-t border-gray-200">{blok.description}</span>}
          </span>
       </SbEditable>
+      
    );
 };
 
 export default BlockDownload;
 
+const getContrast = (hex) => {
+   let r = parseInt(hex.substr(1, 2), 16),
+      g = parseInt(hex.substr(3, 2), 16),
+      b = parseInt(hex.substr(5, 2), 16),
+      yiq = (r * 299 + g * 587 + b * 114) / 1000;
+   return yiq >= 128 ? "black" : "white";
+};
