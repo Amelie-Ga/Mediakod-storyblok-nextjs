d25196b2f6161d6d5a8752cab5ece729981ed342
Amelie-Ga <AmelieGa@outlook.com>
2021-09-16 16:39:47 +0200

    correction

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import DynamicComponent from '../components/DynamicComponent'
+import Link from 'next/link'
 
 import { useStoryblok } from "../utils/storyblok"
 
@@ -9,12 +10,19 @@ export default function Page404({ preview}) {
   // const enableBridge = preview; // load only inside preview mode
   const storyLoaded = useStoryblok(null, enableBridge)
 
-  let content = (<h1>Not found</h1>)
   if(storyLoaded && storyLoaded.content) content = (<DynamicComponent blok={storyLoaded.content} />)
 
   return (
     <Layout>
-      { content }
+      <div className="flex flex-col items-center justify-center">
+        <h2>Page 404</h2>
+        <p className="w-2/3 text-center">La page demandée n’existe pas / plus. Celle-ci a peut-être été renommée ou déplacée. Veuillez nous excuser pour ce désagrément.</p>
+        <Link  href="/">
+          <a className="w-full px-4 py-2 my-8 font-bold text-center text-white bg-gray-500 rounded-md hover:bg-gray-600 md:w-auto md:px-8">
+            Retour à la page d'acceuil
+          </a>
+        </Link>
+      </div>
     </Layout>
   )
 }
