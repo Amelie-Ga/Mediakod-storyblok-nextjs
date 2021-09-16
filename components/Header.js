a86cfb91bca28a623a4e038a504bd9008f5a5093
Amelie-Ga <AmelieGa@outlook.com>
2021-09-16 16:22:24 +0200

    Header correction

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,7 @@ import React,{useState,useEffect} from "react";
 const Header = () => {
 
    const [state, setState] = useState('');
+   const [alt, setAlt] = useState('');
 
 	const getDataState = async () => {
 		return await fetch("https://api.storyblok.com/v2/cdn/stories/logo?version=draft&token=9wcJu9VBMThoyv5u8WBVJwtt&cv=1631800032")
@@ -12,6 +13,7 @@ const Header = () => {
 	useEffect(() => {
 		async function data() {
          setState((await getDataState()).story.content.body[2].image.filename)
+         setAlt((await getDataState()).story.content.body[2].filename)
 		} 
 		data();
 	
@@ -21,7 +23,7 @@ const Header = () => {
       <header className="container block w-full p-0 mx-auto mt-8 bg-fixed bg-white ds-nav">
          <div className="w-full pr-6 sm:w-4/12 md:w-3/12">
             <h1 className="text-lg font-bold text-gray-800">
-               <Link href="/"><a><img src={state} alt=""/></a></Link>
+               <Link href="/"><a><img src={state} alt={alt}/></a></Link>
             </h1>
          </div>
       </header>
